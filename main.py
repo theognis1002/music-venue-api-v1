42396301eaa52e21f14dd0a0b611c2b1aa64705d
theognis1002 <theognis1002@gmail.com>
2020-06-23 11:20:11 -0400

    updated add venue post route and index route

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-import json
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, String, text
 from sqlalchemy.dialects.mysql import INTEGER
 from sqlalchemy.orm import sessionmaker, load_only
-from pprint import pprint
+import json
+
 
 app = Flask(__name__)
 
@@ -26,8 +26,27 @@ class Venue(Base):
     add_method = Column(String(50))
 
 
-@app.route('/venue_api/v1', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
+    """list route endpoints"""
+    endpoints = []
+
+    for r in app.url_map._rules:
+        if r.rule == '/static/<path:filename>':
+            continue
+
+        route = {"route": r.rule,
+                 "description": r.endpoint.replace("_", " "),
+                 "methods": [method for method in list(r.methods) 
+                             if method != 'HEAD' and method != 'OPTIONS']}
+        endpoints.append(route)
+
+    return render_template('index.html', endpoints=endpoints, request=request)
+
+
+@app.route('/venue/v1', methods=['GET'])
+def get_venue():
+    """get venue information from database"""
     try:
         venue_query = request.args.get("venue")
         if venue_query:
@@ -46,9 +65,37 @@ def index():
                 return jsonify([{"error": f"No venue found for following query: '{venue_query}'"}])
 
         else:
-            return jsonify([{"error": f"No venue query requested."}])
+            return jsonify([{"error": f"Please provide valid venue query."}])
+
     except (TypeError, KeyError, AttributeError) as e:
         return jsonify({"error": f"{e.__class__.__name__} - {str(e)}"})
+
+
+@app.route('/venue/v1/add', methods=['POST'])
+def add_venue():
+    """add venue information to database"""
+    if request.method == 'POST':
+        print(request.args)
+        required_args = ['venue', 'capacity', 'location', 'state']
+
+        if all(args in request.args for args in required_args):
+
+            try:
+                new_venue = Venue(request.args.to_dict())
+                session.add(new_venue)
+                session.commit()
+                response = jsonify({"result": "success"})
+
+                response.status_code = 200
+                return response
+
+            except (TypeError, KeyError) as e:
+                response = jsonify({"error": str(e)})
+                response.status_code = 400
+                return response
+
+        else:
+            return jsonify({"error": "missing arg(s)"})
 
 
 if __name__ == '__main__':
